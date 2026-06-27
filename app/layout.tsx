e8525a5e804085b0c5e148905ec56301d5b09659
mohoas95-dot <mohoas95@gmail.com>
2026-06-28 00:16:33 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: 'سامانه هوشمند برنامه‌ریزی شیفت پرستاری بیمارستان',
   description: 'سیستم هوشمند تخصیص خودکار و عادلانه شیفت پرسنل با موتور الگوریتمی پیشرفته و هوشمند',
   themeColor: '#000000',
-  manifest: '/manifest.webmanifest',
+  manifest: '/manifest.json',
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
@@ -23,6 +23,15 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="fa" dir="rtl" className={`${vazirmatn.variable} scroll-smooth`}>
+      <head>
+        {/* آیکون استاندارد اندروید و مرورگرها */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
+        
+        {/* آیکون‌های اختصاصی دستگاه‌های اپل (iOS) */}
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512x512.png" />
+      </head>
       <body suppressHydrationWarning className="font-sans antialiased text-slate-800 bg-slate-50 min-h-screen">
         {children}
       </body>
