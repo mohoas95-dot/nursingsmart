2d9dc28b7cdfd50628010d7fc0cff66a52283cfe
mohoas95-dot <mohoas95@gmail.com>
2026-06-28 23:03:36 +0330

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,3 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// این بخش شرط اصلی کروم برای معتبر شناختن PWA است
 self.addEventListener('fetch', (event) => {
-  // در حال حاضر فقط درخواست‌ها را عبور می‌دهد اما شرط کروم را پاس می‌کند
   event.respondWith(fetch(event.request));
 });
-
